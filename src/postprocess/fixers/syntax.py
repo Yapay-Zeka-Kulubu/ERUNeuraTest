c01a4285a72f82678afa0167b2f872efdde2e37f
İbrahim Mert <akbas.ibrahim641@gmail.com>
2026-03-14 15:08:15 +0300

    Postprocess'teki tüm sınıflara genel amacıyla neler yapılacagı yazıldı. Ayrıca notes/postprocess kısmın eklendi, kodlar açıklamalarıyla buraya kaydedildi.

---
+++ src/postprocess/fixers/syntax.py
@@ -0,0 +1,7 @@
+class SyntaxFixer:
+    """
+    Üretilen test kodundaki syntax hatalarını düzeltmek için kullanılır.
+
+    AST (Abstract Syntax Tree) yardımıyla syntax hataları tespit edilir.
+    Mümkün olan durumlarda kural tabanlı düzeltmeler uygulanır.
+    """
